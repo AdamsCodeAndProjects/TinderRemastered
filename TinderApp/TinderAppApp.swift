d50a74054ea4069bbab76f18439cef6aea614750
Adam Januszewski <Adpjan@att.net>
2022-03-10 11:19:31 -0500

    Initial Commit

---
+++ TinderApp/TinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderAppApp.swift
+//  TinderApp
+//
+//  Created by adam janusewski on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
